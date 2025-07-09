fd14083cd675900a27da3685993b5f8af9c0384f
Dilmuhammad <abdumalikovdilmuhammad06@gmail.com>
2025-07-09 14:10:21 +0500

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Route, Routes } from "react-router";
+import { Navigate, NavLink, Route, Routes } from "react-router";
 import MainLayout from "./layout/Main";
 import React from "react";
 const PostsPage = React.lazy(() => import("./pages/Posts"));
@@ -8,6 +8,7 @@ const UsersPage = React.lazy(() => import("./pages/Users"));
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" />} />
       <Route path="/dashboard" element={<MainLayout />}>
         <Route path="products" element={<ProductsPage />} />
         <Route path="posts" element={<PostsPage />} />
